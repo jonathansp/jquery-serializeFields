01832aa6f2251a45e4fd359d4ab282877c44ff96
crosa <crosa@motorola.com>
2015-06-03 14:43:41 -0300

    Added support to ignore fields and fix checkbox issue
    -- Created new function to parse values by type
    -- Added new default option ignored_fields
    -- Updated parse function to get value through new function
    -- Updated parse function to ignore selected fields

--- src/jquery-serializeFields.js
+++ src/jquery-serializeFields.js
@@ -1,38 +1,39 @@
 /*
- * jquery-serializeFields v0.0.2
+ * jquery-serializeFields v0.0.3
  * https://github.com/jonathansp/serializeFields
  *
  * Copyright (c) 2015
 
  * Jonathan Prates (jonathansp),
  * Gabriel Broilo (broilogabriel)
+ * Cristian Oliveira (CristianOliveiraDaRosa)
 
  * Licensed under GPL license.
  */
-(function ($) {
+(function($) {
 
     "use strict";
 
-    $.fn.serializeFields = function (options) {
+    $.fn.serializeFields = function(options) {
         var sf = $.serializeFields; // namespace
         sf.extendSettings(options);
 
         var response = [];
 
         if (this.length < 1) {
-            return;  // do nothing
+            return; // do nothing
         }
 
         if (this.length === 1) {
             return sf.parse($(this)[0]); // return as object
         }
 
-        this.each(function () {
+        this.each(function() {
             response.push(
-              sf.parse($(this))
+                sf.parse($(this))
             );
         });
-        return response;  /// return as array
+        return response; /// return as array
 
     };
 
@@ -40,79 +41,83 @@
     // and settings
     $.serializeFields = {
 
-      defaultSettings: function() {
-          return {
-              field_selector: "input:enabled",
-              fieldset_selector: "fieldset:enabled",
-              fieldset_nameattr: "data-name",
-              ignored_fields: [
-                "input[type=checkbox]:not(:checked)"
-              ]
-          };
-      },
+        defaultSettings: function() {
+            return {
+                field_selector: "input:enabled",
+                fieldset_selector: "fieldset:enabled",
+                fieldset_nameattr: "data-name",
+                ignored_fields: null
+            };
+        },
 
-      /**
-        Function extend default settings
+        /**
+          Function extend default settings
 
-        param: options
-      **/
+          param: options
+        **/
 
-      extendSettings: function(options){
-        $.serializeFields.settings = $.serializeFields.defaultSettings();
-        $.extend($.serializeFields.settings, options)
-      },
+        extendSettings: function(options) {
+            $.serializeFields.settings = $.serializeFields.defaultSettings();
+            $.extend($.serializeFields.settings, options)
+        },
 
-      /**
-        Function validate ignored fields from settings
+        /**
+         Function to parse value from input by type
 
-        param: element to validate
-        return: true/false
-      **/
-      isIgnored: function(element){
-        var ignore = false;
+         param: element
+         return: value
+        **/
+        getValue: function(element) {
+            var value = $(element).val();
+            var type = $(element).attr("type");
 
-        $($.serializeFields.settings.ignored_fields).each(function(i,ignored){
-            ignore = $(element).is(ignored);
-        })
+            if (type == "checkbox") {
+                return $(element).is(":checked");
+            }
 
-        return ignore;
-      },
+            if ($.isNumeric(value)) {
+                return parseFloat(value);
+            }
 
-      /**
-        Function contain all the logic to parse fields to a JSON object
+            return value;
+        },
 
-        param: element
-        return: json object
-      **/
-      parse: function (element) {
-          var sf = $.serializeFields;
-          var data = {};
+        /**
+          Function contain all the logic to parse fields to a JSON object
 
-          $(element).children().each(function () {
+          param: element
+          return: json object
+        **/
+        parse: function(element) {
+            var sf = $.serializeFields;
+            var data = {};
 
-              if(!sf.isIgnored($(this))){
-                  if ($(this).is(sf.settings.field_selector)) {
-                      var key = $(this).attr("name") || $(this).attr("id");
-                      var value = $(this).val();
+            $(element).children().each(function() {
 
-                      if (key) {
-                          data[key] = value;
-                      }
+                if (!$(this).is(sf.settings.ignored_fields)) {
 
-                  } else {
+                    if ($(this).is(sf.settings.field_selector)) {
+                        var key = $(this).attr("name") || $(this).attr("id");
+                        var value = sf.getValue(this);
 
-                      if ($(this).is(sf.settings.fieldset_selector)) {
-                          var name = $(this).attr(sf.settings.fieldset_nameattr);
-                          if (name) {
-                              data[name] = sf.parse($(this));
-                          }
-                      } else {
-                          $.extend(data, sf.parse($(this)));
-                      }
-                  }
-              }
-          });
-          return data;
-      }
+                        if (key) {
+                            data[key] = value;
+                        }
+
+                    } else {
+
+                        if ($(this).is(sf.settings.fieldset_selector)) {
+                            var name = $(this).attr(sf.settings.fieldset_nameattr);
+                            if (name) {
+                                data[name] = sf.parse($(this));
+                            }
+                        } else {
+                            $.extend(data, sf.parse($(this)));
+                        }
+                    }
+                }
+            });
+            return data;
+        }
     }
 }(jQuery));
