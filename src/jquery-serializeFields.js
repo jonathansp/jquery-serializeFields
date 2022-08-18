5bdc764a8cf3580e274d6e469211d2253b08fbef
chaparo <trashod@hotmail.fr>
2022-08-18 11:56:14 +0200

    Parse strict number on option
    Parse number only if  strict_convert_number_type is activated

--- src/jquery-serializeFields.js
+++ src/jquery-serializeFields.js
@@ -3,14 +3,13 @@
  * https://github.com/jonathansp/serializeFields
  *
  * Copyright (c) 2015-2022.
-
  * Licensed under GPL license.
  */
-(function($) {
+(function ($) {
 
     "use strict";
 
-    $.fn.serializeFields = function(options) {
+    $.fn.serializeFields = function (options) {
         var sf = $.serializeFields; // namespace
         sf.extendSettings(options);
 
@@ -24,7 +23,7 @@
             return sf.parse($(this)[0]); // return as object
         }
 
-        this.each(function() {
+        this.each(function () {
             response.push(
                 sf.parse($(this))
             );
@@ -37,32 +36,32 @@
     // and settings
     $.serializeFields = {
 
-        defaultSettings: function() {
+        defaultSettings: function () {
             return {
                 field_selector: "input:enabled,select:enabled,textarea:enabled",
                 fieldset_selector: "fieldset:enabled",
                 fieldset_nameattr: "data-name",
-                ignored_fields: "input[type='radio']:not(:checked)"
+                ignored_fields: "input[type='radio']:not(:checked)",
+                strict_convert_number_type: "false"
             };
         },
 
         /**
           Function extend default settings
-
           param: options
         **/
-        extendSettings: function(options) {
+        extendSettings: function (options) {
             $.serializeFields.settings = $.serializeFields.defaultSettings();
             $.extend($.serializeFields.settings, options);
         },
 
         /**
           Function to parse value from input by type
-
           param: element
           return: value
         **/
-        getValue: function(element) {
+        getValue: function (element) {
+            var sf = $.serializeFields;
             var value = $(element).val();
             var type = $(element).attr("type");
 
@@ -70,7 +69,7 @@
                 return $(element).is(":checked");
             }
 
-            if ($.isNumeric(value)) {
+            if (sf.settings.strict_convert_number_type === true && $.isNumeric(value)) {
                 return parseFloat(value);
             }
 
@@ -79,15 +78,14 @@
 
         /**
           Function contain all the logic to parse fields to a JSON object
-
           param: element
           return: json object
         **/
-        parse: function(element) {
+        parse: function (element) {
             var sf = $.serializeFields;
             var data = {};
 
-            $(element).children().each(function() {
+            $(element).children().each(function () {
 
                 if (!$(this).is(sf.settings.ignored_fields)) {
 
